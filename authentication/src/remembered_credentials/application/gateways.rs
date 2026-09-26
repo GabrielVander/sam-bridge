c5ac6cd2758397b64d8d4edd8d4c1d7deca46c53
Gabriel Vander <prism1095@duckdns.com>
2026-09-25 21:02:52 -0300

    (general) Remove unused objects

--- authentication/src/remembered_credentials/application/gateways.rs
+++ authentication/src/remembered_credentials/application/gateways.rs
@@ -1,9 +1,7 @@
 mod clear_credential_gateway;
-mod credential_store_error;
 mod load_credential_gateway;
 mod save_credential_gateway;
 
 pub use clear_credential_gateway::*;
-pub use credential_store_error::*;
 pub use load_credential_gateway::*;
 pub use save_credential_gateway::*;
--- authentication/src/remembered_credentials/application/gateways/credential_store_error.rs
+++
@@ -1,7 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug, Clone, Copy)]
-pub enum CredentialStoreError {
-    #[error("Unable to perform credential storage operation")]
-    UnableToPerformOperation,
-}
